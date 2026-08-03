fc4981a817a59a12872f71ef7d21f2e6c1e78a4c
Muchkundraje Thote <146013213+MUCHKUNDRAJE@users.noreply.github.com>
2026-08-03 12:29:37 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+
+      HashMap < Character , Character >  map = new HashMap <>();
+      Stack <Integer> stack = new Stack<>();
+
+      map.put(')','(');
+         map.put(')','(');
+          
+   
+
+
+
+
+
+
+    }
+}
