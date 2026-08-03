1c0cd3efe034c8fb3a934265f3ba87de36205b6c
Muchkundraje Thote <146013213+MUCHKUNDRAJE@users.noreply.github.com>
2026-08-03 13:11:49 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,34 @@
+class MinStack {
+
+    Stack <Integer>stack = new Stack <>();
+    Stack <Integer> minstack = new Stack <>();
+    int min = Integer.MAX_VALUE ;
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        min = Math.min(min , val);
+        minstack.push(min); 
+    
+    }
+    
+    public void pop() {
+        int pop  = stack.pop();
+        int minpop = minstack.pop();
+        if(!minstack.isEmpty())
+        {
+            min = minstack.peek();
+        }
+         
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minstack.peek();
+    }
+}
